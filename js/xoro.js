fde9478cf6205b96b7948be4ec50fc6a26f33028
krifoElectron <ars2d2@yandex.ru>
2019-07-09 10:37:44 +0300

    navigation and fonts were changed

--- js/xoro.js
+++ js/xoro.js
@@ -1,3 +1,23 @@
-var x = 2;
-console.log(x + ' - число, а не текст. жуть какая!');
-alert('Уходи!');
+let user = {
+    name: 'Инокентий',
+  },
+  user2 = {
+    name: 'Яков',
+  };
+
+user.sayHi = function() {
+  alert('Привет, ' + this.name + '!');
+};
+
+user.sayHi();
+
+user2.g = user.sayHi;
+
+user2.g();
+
+// user.name = 'Яков';
+// user.sayHello = () => {
+//   alert('Привет, ' + this.name + '!');
+// };
+
+// user.sayHello();
